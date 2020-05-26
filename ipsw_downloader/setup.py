6577a709b1851b813fe3dc3fe66957a2f58c1ba4
clziegler <clziegler@me.com>
2020-05-25 19:01:14 -0700

    fix setup

--- ipsw_downloader/setup.py
+++ ipsw_downloader/setup.py
@@ -117,12 +117,9 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
     ],
     # $ setup.py publish support.
     cmdclass={
